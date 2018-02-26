4f42dbbeebe936149b8755e06c7ffebd14084839
cfaloon <cfaloon@gmail.com>
2018-02-26 13:31:26 -0500

    Add some Album seeds.
    Albums must be created with an Artist

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap', git: 'https://github.com/twbs/bootstrap-rubygem'
 gem 'devise'
 gem 'jquery-rails'
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+artist = Artist.find_or_create_by(name: 'Umphrey\'s Mcgee')
+30.times { Album.find_or_create_by(name: Faker::UmphreysMcgee.song, artist: artist) }
