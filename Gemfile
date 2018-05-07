508065aa1907a166d1b90605e9f992bc26d843c1
cfaloon <cfaloon@gmail.com>
2018-05-07 17:33:29 -0400

    Start using the database-cleaner Gem throughout tests

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :development do
 end
 
 group :test do
+  gem 'database_cleaner'
   gem 'rails-controller-testing'
 end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,14 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'database_cleaner'
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-  
+
+  # DatabaseCleaner
+  DatabaseCleaner.strategy = :transaction
+  setup { DatabaseCleaner.start }
+  teardown { DatabaseCleaner.clean }
+
   # Add more helper methods to be used by all tests here...
 end
